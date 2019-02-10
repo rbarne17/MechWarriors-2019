1cf0c195ba8f1686db8f35f990c7586a5deca496
Memphis_MechWarriors_4764 <MMW@LIB-CTTXRQ2.memphis.ad>
2019-02-10 17:33:07 -0600

    first commit and adding data from subsystems.

---
+++ src/main/java/frc/robot/Dashboard.java
@@ -0,0 +1,55 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot;
+
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+/**
+ * Add your docs here.
+ */
+public class Dashboard {
+
+    public void robotInit()
+    {
+        		// subsystems display
+		SmartDashboard.putData(Robot.m_drivetrain);
+		SmartDashboard.putData(Robot.m_liftmechanism);
+		SmartDashboard.putData(Robot.m_targetgripper);
+		SmartDashboard.putData(Robot.m_climbhab);
+
+
+    }
+
+    public void teleopPeriodic()
+
+    {
+        // DriveTrain
+        SmartDashboard.putNumber("Percent Throttle", Robot.m_oi.getControllerDriveTrainThrottleValue());
+        SmartDashboard.putNumber("Percent Rotation", Robot.m_oi.getControllerDriveTrainRotationValue());
+        // SmartDashboard.putNumber("GyroHeading", Robot.m_drivetrain.getHeading());
+        SmartDashboard.putNumber("ACTUAL Percent Throttle",
+                Robot.m_drivetrain.scalingSpeed(Robot.m_oi.getControllerDriveTrainThrottleValue()));
+        SmartDashboard.putNumber("ACTUAL Percent Rotation",
+                Robot.m_drivetrain.scalingSpeed(Robot.m_oi.getControllerDriveTrainRotationValue()));
+
+        // LiftMechanism
+        SmartDashboard.putNumber("Lift Encoder", Robot.m_liftmechanism.getEncoderLift());
+        SmartDashboard.putBoolean("LiftAtHome", Robot.m_liftmechanism.liftAtHome());
+        SmartDashboard.putNumber("LiftControllerValue", Robot.m_oi.getControllerLiftValue());
+
+        // TargetGripper
+        SmartDashboard.putBoolean("TargetGripperDown", Robot.m_targetgripper.getTargetGripperDown());
+        SmartDashboard.putBoolean("TargetGripperUp", Robot.m_targetgripper.getTargetGripperUp());
+        SmartDashboard.putBoolean("TargetGripperHatch", Robot.m_targetgripper.getTargetGripperHatch());
+        
+        // ClimbHab
+        // SmartDashboard.putBoolean("ClimbHabUp", Robot.m_climbhab.getClimbHabUp());
+        // SmartDashboard.putBoolean("ClimbHapDown", Robot.m_climbhab.getClimbHabDown());
+
+    }
+}
