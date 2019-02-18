88c95fe166de3ef8a2f294cf5acdd1e73e8f8cd0
Barnes <rbarne17@uthsc.edu>
2019-02-17 19:54:37 -0600

    new commands for specific positions.

---
+++ src/main/java/frc/robot/commands/LiftRocketLevel1Lift.java
@@ -0,0 +1,47 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj.command.Command;
+import frc.robot.Robot;
+import frc.robot.RobotMap;
+
+public class LiftRocketLevel1Lift extends Command {
+  public LiftRocketLevel1Lift() {
+    // Use requires() here to declare subsystem dependencies
+    // eg. requires(chassis);
+  }
+
+  // Called just before this Command runs the first time
+  @Override
+  protected void initialize() {
+  }
+
+  // Called repeatedly when this Command is scheduled to run
+  @Override
+  protected void execute() {
+    Robot.m_lift.setLiftDirection(RobotMap.LIFT_SPEED,RobotMap.LIFT_MECHANISM_ENCODER_ROCKET_LEVEL_1);
+  }
+
+  // Make this return true when this Command no longer needs to run execute()
+  @Override
+  protected boolean isFinished() {
+    return (Robot.m_lift.getEncoderLift() >= RobotMap.LIFT_MECHANISM_ENCODER_ROCKET_LEVEL_1);
+  }
+
+  // Called once after isFinished returns true
+  @Override
+  protected void end() {
+  }
+
+  // Called when another command which requires one or more of the same
+  // subsystems is scheduled to run
+  @Override
+  protected void interrupted() {
+  }
+}
