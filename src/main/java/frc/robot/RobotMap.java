19d03d30ab7ab611dcd9f17c8d92a9046c014f00
Barnes <rbarne17@uthsc.edu>
2019-02-17 19:51:22 -0600

    Lots of cleanup, plus added speed for motors.

--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -7,8 +7,6 @@
 
 package frc.robot;
 
-import edu.wpi.first.wpilibj.DigitalSource;
-
 /**
  * The RobotMap is a mapping from the ports sensors and actuators are wired into
  * to a variable name. This provides flexibility changing wiring, makes checking
@@ -25,48 +23,43 @@ public class RobotMap {
   public static final int DRIVETRAIN_RIGHT_SPARK = 1;
 
   // DIO
-  public static final int LEFT_ENCODER_CHANNEL_1 = 0;
-  public static final int LEFT_ENCODER_CHANNEL_2 = 1;
-  public static final int RIGHT_ENCODER_CHANNEL_1 = 2;
-  public static final int RIGHT_ENCODER_CHANNEL_2 = 3;
-  public static final DigitalSource TARGET_GRIPPER_ENCODER_CHANNEL_1 = null;
-  public static final DigitalSource TARGET_GRIPPER_ENCODER_CHANNEL_2 = null;
+  public static final int DRIVETRAIN_LEFT_ENCODER_CHANNEL_1 = 0;
+  public static final int DRIVETRAIN_LEFT_ENCODER_CHANNEL_2 = 1;
+  public static final int DRIVETRAIN_RIGHT_ENCODER_CHANNEL_1 = 2;
+  public static final int DRIVETRAIN_RIGHT_ENCODER_CHANNEL_2 = 3;
 
   // Solenoid valves
   public static final int CLIMBHAB_SOLENOID_DEPLOY = 0;
-
   public static final int CLIMBHAB_SOLENOID_RETRACT = 1;
 
   // CAN
-  public static final int CLIMBHAB_SOLENOID = 1
-  ;
+  public static final int CLIMBHAB_SOLENOID = 1;
   public static final int POWER_DISTRIBUTION_BOARD = 2;
-
   public static final int LIFT_MECHANISM_MOTOR = 5;
-
   public static final int TARGET_ARM_MOTOR = 6;
-
   public static final int TARGET_GRIPPER_MOTOR = 7;
-
   public static final int TARGET_PULLER_MOTOR = 8;
-
   public static final int TARGET_PUSHER_PULLER_MOTOR = 9;
 
   // Encoder values
   public static final int LIFT_MECHANISM_ENCODER_HOME = 4;
+  public static final int TARGET_GRIPPER_ENCODER_HIGH = 0;
+  public static final int TARGET_GRIPPER_ENCODER_LOW = 0;
+  public static final int TARGET_GRIPPER_ENCODER_HATCH_LOW = 0;
+  public static final int TARGET_ARM_ENCODER_HIGH = 0;
+  public static final int TARGET_ARM_ENCODER_LOW = 0;
+  public static final int TARGET_GRIPPER_ENCODER_HATCH_HIGH = 0;
+  public static final int LIFT_MECHANISM_ENCODER_ROCKET_LEVEL_3 = 0;
+  public static final int LIFT_MECHANISM_ENCODER_CARGO_SHIP = 0;
+  public static final int LIFT_MECHANISM_ENCODER_DEPOT = 0;
+  public static final int LIFT_MECHANISM_ENCODER_LOADING_STATION = 0;
+  public static final int LIFT_MECHANISM_ENCODER_ROCKET_LEVEL_1 = 0;
+  public static final int LIFT_MECHANISM_ENCODER_ROCKET_LEVEL_2 = 0;
 
-  public static final int TARGET_GRIPPER_ENCODER_UP = 0;
-
-  public static final int TARGET_GRIPPER_ENCODER_DOWN = 0;
-
-  public static final int TARGET_GRIPPER_ENCODER_HATCH_LOWER = 0;
-
-  public static final int TARGET_GRIPPER_ENCODER_HATCH_UPPER = 0;
-
-  public static final int TARGET_ARM_ENCODER_UP = 0;
-
-  public static final int TARGET_ARM_ENCODER_DOWN = 0;
-
-  public static final int LIFT_MECHANISM_ENCODER_LEVEL_3 = 0;
-
+  // Motor speeds
+  public static final double LIFT_SPEED = .5;
+  public static final double TARGET_PULLER_SPEED = .5;
+  public static final double TARGET_PUSHER_PULLER_SPEED = .5;
+  public static final double TARGET_GRIPPER_SPEED = .5;
+  public static final double TARGET_ARM_SPEED = .5;
 }
